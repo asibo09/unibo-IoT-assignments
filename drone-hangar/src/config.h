1f7c198741057d9ec668390ebac28748e9e10783
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-04-13 10:31:33 +0200

    Update config with rimanent leds and button pin

--- drone-hangar/src/config.h
+++ drone-hangar/src/config.h
@@ -1,11 +1,16 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
-#define PIN_LED_L2 12
+#define PIN_LED_L1 11 //verde drone inside
+#define PIN_LED_L2 12 //verde blinking
+#define PIN_LED_L3 13 //rosso alarm
+
 #define PIN_SONAR_TRIG 7
 #define PIN_SONAR_ECHO 8
 #define PIN_PIR 2
 #define PIN_TEMP_SENSOR A0
 #define PIN_SERVO 9
+
+#define PIN_BUTTON_RESET 4
 
 #endif
