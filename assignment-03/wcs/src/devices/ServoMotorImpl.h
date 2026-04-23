d68660f2184761b44d68d1745d609a5c631a2dbb
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-04-23 12:07:25 +0200

    Add configuration for Servo Motor in devices folder

---
+++ assignment-03/wcs/src/devices/ServoMotorImpl.h
@@ -0,0 +1,25 @@
+#ifndef __SERVO_MOTOR_IMPL__
+#define __SERVO_MOTOR_IMPL__
+
+#include "ServoMotor.h"
+#include "ServoTimer2.h"
+#include <arduino.h>
+
+
+class ServoMotorImpl : public ServoMotor {
+
+public:
+  ServoMotorImpl(int pin);
+
+  void on();
+  bool isOn();
+  void setPosition(int angle);
+  void off();
+
+private:
+  int pin;
+  bool _on;
+  ServoTimer2 motor;
+};
+
+#endif
