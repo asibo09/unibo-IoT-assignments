2a8270fdc01a04c5c04fe2f76fa379ce546091f6
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-05-22 16:14:43 +0200

    Update TempSensor using voltage-based TMP36 conversion

--- assignment-02/drone-hangar/src/devices/TempSensor.cpp
+++ assignment-02/drone-hangar/src/devices/TempSensor.cpp
@@ -5,7 +5,8 @@ TempSensor::TempSensor(int pin) { this->pin = pin; }
 
 float TempSensor::getTemperature() {
   int analogValue = analogRead(pin);
-  float celsius =
-      1 / (log(1 / (1023. / analogValue - 1)) / 3950.0 + 1.0 / 298.15) - 273.15;
+  float voltage = (analogValue / 1024.0) * 5.0;
+  float celsius = (voltage - 0.5) * 100.0;
+
   return celsius;
 }
